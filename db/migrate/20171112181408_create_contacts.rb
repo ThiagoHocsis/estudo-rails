fc59073301cdace3aec2fdfcecb49ddd4223515c
thiagohocsis <thiagohocsis@gmail.com>
2017-11-12 19:45:26 -0200

    adiciona migrations

--- db/migrate/20171112181408_create_contacts.rb
+++ db/migrate/20171112181408_create_contacts.rb
@@ -1,7 +1,7 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-      t.string, :name
+      t.string :name
       t.string :email
       t.references :kind, index: true, foreign_key: true
       t.text :rmk
